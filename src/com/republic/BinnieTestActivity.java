564aedbe0208f2f9372700698ab90ffa946c1959
Ian Childs <imbchilds@gmail.com>
2012-09-22 17:24:09 +0100

    Test Binnie

---
+++ src/com/republic/BinnieTestActivity.java
@@ -0,0 +1,17 @@
+package com.republic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Binnie
+ * Date: 22/09/12
+ * Time: 17:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class BinnieTestActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ src/com/republic/CameraActivity.java
@@ -0,0 +1,17 @@
+package com.republic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Binnie
+ * Date: 22/09/12
+ * Time: 17:17
+ * To change this template use File | Settings | File Templates.
+ */
+public class CameraActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
