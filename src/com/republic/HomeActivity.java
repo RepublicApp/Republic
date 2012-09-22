70a14cafd1991e1bd32c5dd59569ef7243e49d47
Chris Knott <chrisknott@hotmail.co.uk>
2012-09-22 17:11:01 +0100

    Home list

--- src/com/republic/HomeActivity.java
+++ src/com/republic/HomeActivity.java
@@ -9,7 +9,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class HomeActivity extends Activity {
     private ArrayList<Class> activities = new ArrayList<Class>();
@@ -19,8 +18,11 @@ public class HomeActivity extends Activity {
         setContentView(R.layout.home);
 
         ArrayList<String> homeList = new ArrayList<String>();
+        homeList.add("User Profile");
+        activities.add(ProfileActivity.class);
         homeList.add("Map");
-        homeList.add("Profile");
+        homeList.add("Pub page example");
+        homeList.add("Beer page example");
         homeList.add("Camera");
         homeList.add("HTTP Stuff");
         homeList.add("Binnie Tests");
@@ -29,13 +31,21 @@ public class HomeActivity extends Activity {
         ListView homeListView = (ListView)findViewById(R.id.home_list);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(HomeActivity.this, android.R.layout.simple_list_item_1, homeList);
         homeListView.setAdapter(adapter);
-        homeListView.setOnItemClickListener(new HomeListListener());
+        homeListView.setOnItemClickListener(new HomeListListener(this));
     }
 
     private class HomeListListener implements AdapterView.OnItemClickListener{
+        HomeActivity parent;
+
+        public HomeListListener(HomeActivity parent){
+            this.parent = parent;
+        }
+
         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-            //Intent intent = new Intent(this, ProfileActivity.class);     // Make a new intent with Display activity
-            //startActivity(intent);
+            if(activities.contains(i)){
+                Intent intent = new Intent(parent, activities.get(i));
+                startActivity(intent);
+            }
         }
     }
 }
